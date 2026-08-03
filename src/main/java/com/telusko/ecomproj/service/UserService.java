bda5b30913210e715467a77cf1657d63e9fa3fb8
Charith Abbineni <f20230212@hyderabad.bits-pilani.ac.in>
2026-08-03 18:52:19 +0530

    Added BCrypt password hashing

--- src/main/java/com/telusko/ecomproj/service/UserService.java
+++ src/main/java/com/telusko/ecomproj/service/UserService.java
@@ -3,6 +3,7 @@ package com.telusko.ecomproj.service;
 import com.telusko.ecomproj.model.User;
 import com.telusko.ecomproj.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,13 @@ public class UserService {
     @Autowired
     private UserRepo repo;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User register(User user) {
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return repo.save(user);
     }
 
